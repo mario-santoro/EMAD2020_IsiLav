be08d93ef45c1664d6ae7efe1d3897c5645227d8
Mario Santoro <43414100+mario-santoro@users.noreply.github.com>
2020-11-29 13:24:21 +0100

    aggiunto link cambio password

--- src/components/screens/LoginScreen.js
+++ src/components/screens/LoginScreen.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { View, StatusBar } from 'react-native';
+import React, { useState } from 'react';
+import { Text, StyleSheet,  View, StatusBar } from 'react-native';
+import { Link } from '@react-navigation/native';
 import EmailTextInput from '../components/EmailTextInput';
 import PasswordTextInput from '../components/PasswordTextInput';
 import GenericButton from '../components/GenericButton';
 import SignupButton from '../components/SignupButton';
 import LogoNome from '../components/LogoNome';
 const LoginScreen = ({navigation}) => {
-
+  
   return (
     <View
     style={{flex: 1, alignItems: "center", justifyContent: 'center', backgroundColor: 'white'}}
@@ -16,17 +17,30 @@ const LoginScreen = ({navigation}) => {
       barStyle="light-content"
       />
 
-     <LogoNome></LogoNome>
+     <LogoNome/>
 
       <EmailTextInput placeholder="Email" ic="mail"/>
       <PasswordTextInput placeholder="Password"/>
       
       <GenericButton testo="Accedi" onPress={() => navigation.navigate('Home')} />
       <SignupButton onPress={() => navigation.navigate('SignUp')} />
+      
+      <Link style={styles.linkP} to="/RetrivePassw">Hai dimenticato la password?</Link>
     </View>
   );
 };
+const styles = StyleSheet.create({
 
+
+  linkP: {
+    
+    color:"#6AA84F",
+    marginTop:10,
+
+  },
+ 
+
+});
 
  
 export default LoginScreen;
