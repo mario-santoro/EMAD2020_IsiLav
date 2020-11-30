10f4693b3168af4ba03688d5a1858cac26a35eff
Mario Santoro <43414100+mario-santoro@users.noreply.github.com>
2020-11-30 15:32:47 +0100

    aggiunte schermate al navigation

--- App.js
+++ App.js
@@ -6,6 +6,10 @@ import LoginScreen from "./src/screens/LoginScreen";
 import HomeScreen from "./src/screens/HomeScreen";
 import SignUpScreen from './src/screens/SignUpScreen';
 import ConfirmSignUpScreen from './src/screens/ConfirmSignUpScreen';
+import RetrivePassw from './src/screens/RetrivePassw';
+import ChangePassw from './src/screens/ChangePassw';
+import MyProfile from './src/screens/MyProfileScreen';
+import TermAndCondition from './src/screens/TermEConditionScreen';
 const RootStack = createStackNavigator();
  
 const App = () => {
@@ -16,6 +20,10 @@ const App = () => {
         <RootStack.Screen name="Home" component={HomeScreen} />
         <RootStack.Screen name="SignUp" component={SignUpScreen} options={{headerShown: false}}/>
         <RootStack.Screen name="ConfirmSignUp" component={ConfirmSignUpScreen} options={{headerShown: false}}/>
+        <RootStack.Screen name="RetrivePassw" component={RetrivePassw} options={{headerShown: false}}/>
+        <RootStack.Screen name="ChangePassw" component={ChangePassw} options={{headerShown: false}}/>
+        <RootStack.Screen name="MyProfile" component={MyProfile} options={{headerShown: false}}/>
+        <RootStack.Screen name="TermAndCondition" component={TermAndCondition} options={{headerShown: false}}/>
       </RootStack.Navigator>
     </NavigationContainer>
   );
