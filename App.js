a0f6484ffcba0d54917217bfcb2ae6d188be6403
Mario Santoro <43414100+mario-santoro@users.noreply.github.com>
2020-12-03 12:51:36 +0100

    nuove pagine per la navigation

--- App.js
+++ App.js
@@ -10,6 +10,10 @@ import RetrivePassw from './src/screens/RetrivePassw';
 import ChangePassw from './src/screens/ChangePassw';
 import MyProfile from './src/screens/MyProfileScreen';
 import TermAndCondition from './src/screens/TermEConditionScreen';
+import ListaOrdini from './src/screens/ListaOrdiniScreen';
+import ChangePassword2 from './src/screens/ChangePassword2';
+import SuccessScreen from './src/screens/SuccessScreen';
+import ChangeAnagrafica from './src/screens/ChangeAnagrafica';
 const RootStack = createStackNavigator();
  
 const App = () => {
@@ -17,13 +21,17 @@ const App = () => {
     <NavigationContainer>
       <RootStack.Navigator>
         <RootStack.Screen name="Login" component={LoginScreen} options={{headerShown: false}} />
-        <RootStack.Screen name="Home" component={HomeScreen} />
+        <RootStack.Screen name="Home" component={HomeScreen} options={{headerShown: false}}/>
         <RootStack.Screen name="SignUp" component={SignUpScreen} options={{headerShown: false}}/>
         <RootStack.Screen name="ConfirmSignUp" component={ConfirmSignUpScreen} options={{headerShown: false}}/>
         <RootStack.Screen name="RetrivePassw" component={RetrivePassw} options={{headerShown: false}}/>
         <RootStack.Screen name="ChangePassw" component={ChangePassw} options={{headerShown: false}}/>
         <RootStack.Screen name="MyProfile" component={MyProfile} options={{headerShown: false}}/>
         <RootStack.Screen name="TermAndCondition" component={TermAndCondition} options={{headerShown: false}}/>
+        <RootStack.Screen name="ListaOrdini" component={ListaOrdini} options={{headerShown: false}}/>
+        <RootStack.Screen name="ChangePassword2" component={ChangePassword2} options={{headerShown: false}}/>
+        <RootStack.Screen name="SuccessScreen" component={SuccessScreen} options={{headerShown: false}}/>
+        <RootStack.Screen name="ChangeAnagrafica" component={ChangeAnagrafica} options={{headerShown: false}}/>
       </RootStack.Navigator>
     </NavigationContainer>
   );
